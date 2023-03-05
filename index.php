83fd5ae0d4a97c9b3f497d2b999827d7bb2e5195
rani-mishra <89419221+rani-mishra@users.noreply.github.com>
2023-03-05 21:00:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo '<h1>Hello World! My name is Rani how are u ?? I am fine ..Tell me anything About Yourself?? Tell me  something please!!! okyyyy byyyeee take care </h1><h3>Welcome to WPMU DEV</h3>'; ?>
+  <?php echo '<h1>Hello World! My name is Rani how are u ?? I am fine ..Tell me anything About Yourself??  </h1><h3>Welcome to WPMU DEV</h3>'; ?>
  </body>
 </html>
